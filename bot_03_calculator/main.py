520b3fc0235431dc07941d9c0f1be9c4da15f3c2
abhi-saurav-saroya <abhisauravsaroya06@gmail.com>
2026-02-28 17:03:33 +0530

    expression tokenization implemented in calculator bot

---
+++ bot_03_calculator/main.py
@@ -0,0 +1,77 @@
+from dotenv import load_dotenv
+import os
+from telegram import Update
+from telegram.ext import (
+    MessageHandler,
+    CommandHandler,
+    ApplicationBuilder,
+    ContextTypes,
+    filters
+)
+import re
+
+load_dotenv()
+token = os.getenv("BOT_TOKEN")
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        "HEY THERE!\n"
+        "I AM A CALCULATOR."
+    )
+
+def is_tokens_valid(tokens) -> bool:
+    if not tokens:
+        return False
+
+    prev = None
+    balance = 0
+
+    for token in tokens:
+        if token.isdigit():
+            if prev is not None and prev.isdigit():
+                return False
+            if prev == ")":
+                return False
+
+        elif token in ["+", "-", "*", "/"]:
+            if prev in ["+", "-", "*", "/", "(", ")", None]:
+                return False
+
+        elif token == "(":
+            balance += 1
+
+        elif token == ")":
+            balance -= 1
+            if balance < 0:
+                return False
+
+        prev = token
+
+    if prev in ["+", "-", "*", "/", "("]:
+        return False
+
+    return balance == 0
+
+
+async def calculator(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    expression = update.message.text
+
+    if not re.fullmatch(r'[0-9+\-*/()\s]+', expression):
+        await update.message.reply_text("Only integers and + - * / ( ) are allowed.")
+        return
+
+    tokens = re.findall(r'\d+|[+\-*/()]', expression)
+
+    print(tokens)
+
+    if not is_tokens_valid(tokens):
+        await update.message.reply_text("The expression you entered is invalid.")
+        return
+
+
+app = ApplicationBuilder().token(token).build()
+app.add_handler(CommandHandler("start", start))
+app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, calculator))
+
+print("Bot is running...")
+app.run_polling()
