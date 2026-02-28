845591a294be222631ed4ac9ccb1c465c6553145
abhi-saurav-saroya <abhisauravsaroya06@gmail.com>
2026-02-28 18:11:30 +0530

    stack based calculation implemented and bot finalized

--- bot_03_calculator/main.py
+++ bot_03_calculator/main.py
@@ -13,6 +13,13 @@ import re
 load_dotenv()
 token = os.getenv("BOT_TOKEN")
 
+precedence = {
+    '+': 1,
+    '-': 1,
+    '*': 2,
+    '/': 2
+}
+
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(
         "HEY THERE!\n"
@@ -34,7 +41,7 @@ def is_tokens_valid(tokens) -> bool:
                 return False
 
         elif token in ["+", "-", "*", "/"]:
-            if prev in ["+", "-", "*", "/", "(", ")", None]:
+            if prev in ["+", "-", "*", "/", "(" , None]:
                 return False
 
         elif token == "(":
@@ -52,6 +59,82 @@ def is_tokens_valid(tokens) -> bool:
 
     return balance == 0
 
+def calculate(tokens) -> int:
+    tokens.insert(0, "(")
+    tokens.append(")")
+    Values = []
+    Operators = []
+
+    for token in tokens:
+        if token.isdigit():
+            Values.append(int(token))
+
+        elif token in ["+", "-", "*", "/"]:
+            while (
+                len(Operators) > 0
+                and Operators[-1] != "("
+                and precedence[token] <= precedence[Operators[-1]]
+            ):
+                val2 = Values.pop()
+                val1 = Values.pop()
+                op = Operators.pop()
+
+                result = None
+                if op == "+":
+                    result = val1 + val2
+                elif op == "-":
+                    result = val1 - val2
+                elif op == "*":
+                    result = val1 * val2
+                elif op == "/":
+                    result = val1 / val2
+
+                Values.append(result)
+
+            # IMPORTANT: push operator AFTER applying
+            Operators.append(token)
+
+        elif token == "(":
+            Operators.append(token)
+
+        elif token == ")":
+            while len(Operators) and Operators[-1] != "(":
+                val2 = Values.pop()
+                val1 = Values.pop()
+                op = Operators.pop()
+
+                if op == "+":
+                    result = val1 + val2
+                elif op == "-":
+                    result = val1 - val2
+                elif op == "*":
+                    result = val1 * val2
+                elif op == "/":
+                    result = val1 / val2
+
+                Values.append(result)
+
+            # Remove "("
+            Operators.pop()
+
+    while Operators:
+        val2 = Values.pop()
+        val1 = Values.pop()
+        op = Operators.pop()
+
+        if op == "+":
+            result = val1 + val2
+        elif op == "-":
+            result = val1 - val2
+        elif op == "*":
+            result = val1 * val2
+        elif op == "/":
+            result = val1 / val2
+
+        Values.append(result)
+
+    return Values[-1]
+
 
 async def calculator(update: Update, context: ContextTypes.DEFAULT_TYPE):
     expression = update.message.text
@@ -68,6 +151,8 @@ async def calculator(update: Update, context: ContextTypes.DEFAULT_TYPE):
         await update.message.reply_text("The expression you entered is invalid.")
         return
 
+    answer = calculate(tokens)
+    await update.message.reply_text(f"{expression} = {str(answer)}")
 
 app = ApplicationBuilder().token(token).build()
 app.add_handler(CommandHandler("start", start))
